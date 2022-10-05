31ae8f4c2e475e01526ca7438b230fc4bedf043e
Yoko Harada <yokolet@gmail.com>
2022-10-05 16:27:22 +0900

    adds sql statements for finding segments by geojson boundary

--- test/test_example.py
+++ test/test_example.py
@@ -166,6 +166,34 @@ class TestExample(DbTest):
         )
 
         sql = """
+        SELECT id
+        FROM japan_segments
+        WHERE ST_Contains(ST_GeomFromGeoJSON('{
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              130.27313232421875,
+              30.519681272749402
+            ],
+            [
+              131.02020263671875,
+              30.519681272749402
+            ],
+            [
+              131.02020263671875,
+              30.80909017893796
+            ],
+            [
+              130.27313232421875,
+              30.80909017893796
+            ],
+            [
+              130.27313232421875,
+              30.519681272749402
+            ]
+          ]
+        ]}')::geography::geometry, bounds);
         """
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(sql)
